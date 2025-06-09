9dfd3b68a356bfffacb61e1aa192af17b4e1553c
SNicP <p375297200586@gmail.com>
2025-06-09 21:40:55 +0300

    Stub and tests for add

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,5 +1,31 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class PhoneBookTest {
+
+    PhoneBook phoneBook = new PhoneBook();
+
+    @BeforeEach
+    public void fill() {
+        //given
+        phoneBook.add("Николай" , "111222333");
+        phoneBook.add("Иван" , "222333444");
+        phoneBook.add("Наталья" , "333444555");
+    }
+
+    @Test
+    public void addTest(){
+        //given
+        int numberOfContactsExpected = 4;
+
+        //when
+        int numberOfContactsActual = phoneBook.add("Анастасия" , "555666777");
+
+        //then
+        Assertions.assertEquals(numberOfContactsExpected, numberOfContactsActual);
+    }
 
 }
