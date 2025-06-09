4bf85bedeeea629a9e413fe96025c9d6b3faafef
SNicP <p375297200586@gmail.com>
2025-06-09 21:41:59 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,9 +1,18 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
 
+    private static final Map<String,String> phoneBookByName = new TreeMap<>();
+    private static final Map<String,String> phoneBookByNumber = new HashMap<>();
+
     public int add (String name, String numberPhone) {
-        return 0;
+        phoneBookByName.put(name, numberPhone);
+        phoneBookByNumber.put(numberPhone, name);
+        return phoneBookByName.size();
     }
 
 }
